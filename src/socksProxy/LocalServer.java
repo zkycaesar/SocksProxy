85aa019e2ff6d3bfd10e5a4864fc450e69b56daf
ZhangKaiyu <kaiyu.zhang@viewsources.com>
2017-07-04 19:07:38 +0800

    继续开发。

--- src/socksProxy/LocalServer.java
+++ src/socksProxy/LocalServer.java
@@ -14,13 +14,17 @@ import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Map;
+import java.util.Set;
 
 public class LocalServer {
 	private final int PORT = 1070;
 	private Selector selector;
-	private Map<SelectionKey, SelectionKey> cache;
+	ByteBuffer readBuffer;
+	private HashMap<SocketChannel, SocketChannel> cache;
+	private static HashSet<String> httpRequestTable = new HashSet<String>(){{add("GET"); add("POST"); add("CONNECT");add("HEAD");add("OPTIONS"); add("PUT"); add("DELETE"); add("TRACE");}};
 	
 	public static void main(String[] args) {
 		LocalServer ls = new LocalServer();
@@ -36,18 +40,24 @@ public class LocalServer {
 			serverSocketChannel.socket().bind(new InetSocketAddress(PORT));
 			selector = Selector.open();
 			serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+			readBuffer = ByteBuffer.allocate(2048);
+			cache = new HashMap<SocketChannel, SocketChannel>();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
 	private void select(){
+		int count = 0;
 		while(true){
 			try {
 				selector.select();
+				count++;
+				System.out.println(count);
 				Iterator it = selector.selectedKeys().iterator();
 				while(it.hasNext()){
 					SelectionKey selectionKey = (SelectionKey)it.next();
+					it.remove();
 					handleKey(selectionKey);
 				}
 			} catch (IOException e) {
@@ -64,32 +74,76 @@ public class LocalServer {
 			try {
 				while(socketChannel == null){
 					socketChannel = serverSocketChannel.accept();
-					Thread.sleep(300);
+//					Thread.sleep(300);
 				}				
 				socketChannel.configureBlocking(false);
 				socketChannel.register(selector, SelectionKey.OP_READ);
 				System.out.println("Accept socket: " + socketChannel.getRemoteAddress());
-			} catch (IOException | InterruptedException e) {
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 		else if(selectionKey.isReadable()){
 			socketChannel = (SocketChannel)selectionKey.channel();
 			Charset cs = Charset.forName ("UTF-8");
-			ByteBuffer readBuffer;
-			readBuffer = ByteBuffer.allocate(2048);
-			
+			readBuffer.clear();
 			try {
-				System.out.println("Read socket: " + socketChannel.getRemoteAddress());
-//				int count;
+//				System.out.println("Read socket: " + socketChannel.getRemoteAddress());	
 				int count = socketChannel.read(readBuffer);
-//				while((count = socketChannel.read(readBuffer)) != -1){
 				if(count > 0) {
 					readBuffer.flip();
 					CharBuffer charBuffer = cs.decode(readBuffer); 
+					String content = charBuffer.toString();
 		            System.out.println(socketChannel.getRemoteAddress());
-					System.out.println(charBuffer);
-					readBuffer.clear();
+					System.out.println(content);
+					
+					String[] header = content.split(System.getProperty("line.separator"));
+					if(content.startsWith("CONNECT")){						
+						String remoteHost = header[1].substring(6);
+						int index = remoteHost.indexOf(":");
+						if(index != -1)remoteHost = remoteHost.substring(0, index);
+						if(!remoteHost.equals("bbs.sjtu.edu.cn"))return;
+						System.out.println("Remote host: " + remoteHost);
+						SocketChannel remoteSocket = null;
+						remoteSocket = SocketChannel.open();
+						remoteSocket.connect(new InetSocketAddress(remoteHost, 443));
+						remoteSocket.configureBlocking(false);
+						remoteSocket.register(selector, SelectionKey.OP_READ);
+						
+						cache.put(socketChannel, remoteSocket);
+						cache.put(remoteSocket, socketChannel);
+						readBuffer.clear();
+						readBuffer.put(cs.encode(CharBuffer.wrap("HTTP/1.1 200 Connection established" + System.getProperty("line.separator") + System.getProperty("line.separator"))));
+						readBuffer.flip();
+						while(readBuffer.hasRemaining())socketChannel.write(readBuffer);
+					}
+					else{
+						System.out.println("!!!!!!!!!!!!");
+						SocketChannel remoteSocket = cache.get(socketChannel);						
+						if(remoteSocket == null){
+							String remoteHost = null;
+							for(String item : header){
+								if(item.startsWith("Host")){
+									remoteHost = item.substring(6);
+									int index = remoteHost.indexOf(":");
+									if(index != -1)remoteHost = remoteHost.substring(0, index);
+									break;
+								}
+							}
+							remoteSocket = SocketChannel.open();
+							remoteSocket.connect(new InetSocketAddress(remoteHost, 80));
+							remoteSocket.configureBlocking(false);
+							remoteSocket.register(selector, SelectionKey.OP_READ);
+						}
+						remoteSocket.write(readBuffer);
+						while (socketChannel.read(readBuffer) >= 0 || readBuffer.remaining() != 0) {
+//							System.out.println("...............");
+							readBuffer.flip();
+							remoteSocket.write(readBuffer);
+						    readBuffer.compact();    // In case of partial write
+						}	
+						System.out.println("Writing to: " + remoteSocket.getRemoteAddress());
+					}
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -98,6 +152,7 @@ public class LocalServer {
 			
 		}
 	}
+	
 }
 
 //public class LocalServer {
